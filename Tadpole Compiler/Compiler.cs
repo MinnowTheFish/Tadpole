134475cf5e9c6633bd714a6adfca829d1200383d
Daniel Fleming <39723774+MinnowTheFish@users.noreply.github.com>
2018-09-28 19:05:52 +1200

    Changed Complier.cs
    Added some code...

--- Tadpole Compiler/Compiler.cs
+++ Tadpole Compiler/Compiler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +18,24 @@ namespace Tadpole_Compiler
             InitializeComponent();
         }
 
+        string[] fileContents;
+
         public void convertFile(string directory, string type)
         {
+            fileContents = File.ReadAllLines(directory);
             switch (type)
             {
                 case "ProjectFile":
+                    for (int line = 0; line < fileContents.Length; line++)
+                    {
+                        fileContents[line] = fileContents[line].Replace(" ", "").Replace("project{", "");
+                        shift();
+
+                        if (fileContents[line].Contains(""))
+                        {
+
+                        }
+                    }
                     break;
                 case "Form":
                     break;
@@ -38,6 +52,15 @@ namespace Tadpole_Compiler
             {
                 convertFile(chooseFileDialog.FileName, "ProjectFile");
             }
+        }
+
+        private void shift()
+        {
+            for (int index = 0; index < fileContents.Length - 1; index++)
+            {
+                fileContents[index] = fileContents[index + 1];
+            }
+            fileContents[fileContents.Length - 1] = null;
         }
     }
 }
